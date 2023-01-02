f071e0fc08beca4ac85397be308bb7649d008214
Rabah Boulhares <rabah.boulharespro@outlook.fr>
2023-01-02 17:08:59 +0100

    Blog APp finished

---
+++ src/context/BlogContext.js
@@ -0,0 +1,59 @@
+import { call } from "react-native-reanimated";
+import createDataContext from "./createDataContext";
+
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case "edit_blogpost":
+      return state.map((blogPost) => {
+        if (blogPost.id === action.payload.id) {
+          return action.payload;
+        } else {
+          return blogPost;
+        }
+      });
+    case "add_blogpost":
+      return [
+        ...state,
+        {
+          id: Math.floor(Math.random() * 99999),
+          title: action.payload.title,
+          content: action.payload.content,
+        },
+      ];
+    case "delete_blogpost":
+      return state.filter((blogPost) => blogPost.id !== action.payload);
+    default:
+      return state;
+  }
+};
+
+const addBlogPost = (dispatch) => {
+  return (title, content, callback) => {
+    dispatch({ type: "add_blogpost", payload: { title, content } });
+    callback();
+  };
+};
+
+const deleteBlogPost = (dispatch) => {
+  return (id) => {
+    dispatch({ type: "delete_blogpost", payload: id });
+  };
+};
+
+const editBlogPost = (dispatch) => {
+  return (title, content, id, callback) => {
+    dispatch({
+      type: "dit_blogpost",
+      payload: { id, title, content },
+    });
+    if (callback) {
+      callback();
+    }
+  };
+};
+
+export const { Context, Provider } = createDataContext(
+  blogReducer,
+  { addBlogPost, deleteBlogPost, editBlogPost },
+  [{ title: "TEST POST", content: "TEST CONTENT", id: 1 }]
+);
